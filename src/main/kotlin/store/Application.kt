2987d9f1c9111fc99310ddf599fcb4e5f695377a
oungsi2000 <oungsi1000@gmail.com>
2024-11-08 22:35:54 +0900

    feat: 1. 재고 관리 기능 완성
    재고 관리 기능을 완성하였습니다
    추가된 클래스 : InventoryManager

--- src/main/kotlin/store/Application.kt
+++ src/main/kotlin/store/Application.kt
@@ -1,5 +1,135 @@
 package store
 
+import java.nio.file.Paths
+import java.nio.file.Files
+
+enum class ProductsColumn {
+	NAME,
+	PRICE,
+	QUANTITY,
+	PROMOTION
+}
+
+enum class PromotionsColumn {
+	NAME, BUY, GET, START_DATE, END_DATE
+}
+
+class InventoryManager(var inventory:MutableList<MutableMap<ProductsColumn, String>>) {
+	init {
+		checkInventoryType()
+		checkInventoryDuplex()
+	}
+
+	private fun checkInventoryType() {
+		try {
+			inventory.map {
+				it[ProductsColumn.NAME]!!.toString()
+				2.toUInt() / it[ProductsColumn.PRICE]?.toUInt()!! // 가격은 0이 될 수 없습니다
+				it[ProductsColumn.QUANTITY]?.toUInt()
+				it[ProductsColumn.PROMOTION]!!.toString()
+			}
+		} catch (exception: Exception) {
+			throw IllegalArgumentException(Msg.ILLEGAL_TYPE.msg)
+		}
+	}
+
+	private fun checkInventoryDuplex() {
+		var productNamesNotDuplex: MutableSet<String?> = mutableSetOf()
+
+		inventory.map { productNamesNotDuplex.add(it[ProductsColumn.NAME]) }
+
+		for (key in productNamesNotDuplex) {
+			var matches = inventory.filter { it[ProductsColumn.NAME] == key }
+			var normalProduct = matches.filter { it[ProductsColumn.PROMOTION]?.trim() == "null" }
+			var promotionProduct = matches.filter { it[ProductsColumn.PROMOTION]?.trim() != "null" }
+
+			if (normalProduct.size > 1) { throw IllegalArgumentException(Msg.DUPLEX_NAME.msg) }
+			if (promotionProduct.size > 1) { throw IllegalArgumentException(Msg.MULTIPLE_PROMOTION.msg) }
+			if (normalProduct.isEmpty()) { throw IllegalArgumentException(Msg.NO_NORMAL_PRODUCT.msg) }
+		}
+	}
+
+	fun checkProductAvailable(targetProduct:String):Boolean {
+		var isNormalProductAvailable = false
+
+		for (productInfo in inventory) {
+			var productName = productInfo[ProductsColumn.NAME]
+			var productStock = productInfo[ProductsColumn.QUANTITY]
+
+			if (productName == targetProduct && productInfo[ProductsColumn.PROMOTION] == "null") {
+				isNormalProductAvailable = productStock?.toInt()!! > 0
+			}
+		}
+		return isNormalProductAvailable
+	}
+
+
+	fun deduct(boughtProduct:String, boughtAmount:Int, isPromotion:Boolean=false):Msg {
+		var productInfo = inventory.withIndex().filter {
+			it.value[ProductsColumn.NAME] == boughtProduct && it.value[ProductsColumn.PROMOTION] == "null"
+		}
+
+		if (isPromotion) {
+			productInfo = inventory.withIndex().filter {
+				it.value[ProductsColumn.NAME] == boughtProduct && it.value[ProductsColumn.PROMOTION] != "null"
+			}
+		}
+
+		if (productInfo.isEmpty()) {
+			return Msg.PRODUCT_NOT_FOUND
+		}
+
+		var index = productInfo[0].index
+		var productStock = productInfo[0].value[ProductsColumn.QUANTITY]?.toInt()!!
+		var currentStock = productStock - boughtAmount
+
+		if (currentStock < 0 ) {
+			return Msg.OUT_OF_STOCK
+		}
+
+		inventory[index][ProductsColumn.QUANTITY] = currentStock.toString()
+
+		return Msg.SUCCESS
+	}
+
+	 fun inventoryToCSV():String {
+		var inventoryCSV = "name,price,quantity,promotion\n"
+		for (productInfo in inventory) {
+			var text = productInfo.values.toList().joinToString(",") + "\n"
+			inventoryCSV += text
+		}
+		return inventoryCSV
+	}
+
+	fun dumpInventory() {
+		//파일 입출력을 이용하여 inventory 변수의 내용을 덮어씌웁니다
+		Files.write(Paths.get(PRODUCTS_DIR), inventoryToCSV().toByteArray())
+	}
+
+	companion object {
+		val PRODUCTS_DIR = "src/main/resources/products.md"
+		val PROMOTIONS_DIR = "src/main/resources/promotions.md"
+	}
+
+	enum class Msg (val msg:String, val isSuccess: Boolean) {
+		PRODUCT_NOT_FOUND("상품을 찾을 수 없습니다", false),
+		OUT_OF_STOCK("재고가 부족합니다", false),
+		SUCCESS("재고 변경에 성공하였습니다", true),
+		ILLEGAL_TYPE("재고 타입이 올바른 타입이 아닙니다", false),
+		DUPLEX_NAME("중복된 상품이 존재합니다", false),
+		MULTIPLE_PROMOTION("한 상품에 두 가지 이상의 프로모션이 존재합니다", false),
+		NO_NORMAL_PRODUCT("일반 상품 없이 프로모션 정보를 가질 수 없습니다. 일반 상품 재고가 없다면 꼭 0을 입력해주세요", false)
+
+	}
+}
+
+class InputView {
+	fun readItem() {
+	//프로모션 재고의 이름은 내부적으로 상품 이름 + [프로모션] 이 붙게끔 설정합니다
+	}
+}
+
 fun main() {
-    // TODO: 프로그램 구현
+	// TODO: 프로그램 구현
+
 }
--- src/test/kotlin/store/ApplicationTest.kt
+++ src/test/kotlin/store/ApplicationTest.kt
@@ -4,7 +4,13 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertNowTest
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.MethodSource
 import java.time.LocalDate
 
 class ApplicationTest : NsTest() {
@@ -58,6 +64,189 @@ class ApplicationTest : NsTest() {
             assertThat(output()).contains("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.")
         }
     }
+
+    override fun runMain() {
+        main()
+    }
+}
+
+class InventoryClassTest : NsTest() {
+
+    @Test
+    fun `재고 목록의 유효성을 검사한다`() {
+        var inventory = mutableListOf(
+            mutableMapOf(
+                ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "0001",
+                ProductsColumn.QUANTITY to "0",
+                ProductsColumn.PROMOTION to "null"),
+            mutableMapOf(
+                ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "10",
+                ProductsColumn.PROMOTION to "dd"),
+            )
+        assertDoesNotThrow {
+            InventoryManager(inventory)
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = [
+        "콜라:1000:-1:null",
+        "콜라:-1:43:null",
+        "콜라:0:0:프로모션"
+                       ],
+        delimiter = ':')
+    fun `재고 목록이 타입과 일치하지 않을 시, IllegalArgumentException을 반환한다`(a:String, b:String, c:String, d:String) {
+        var inventory = mutableListOf(
+            mutableMapOf(
+                ProductsColumn.NAME to a,
+                ProductsColumn.PRICE to b,
+                ProductsColumn.QUANTITY to c,
+                ProductsColumn.PROMOTION to d),
+        )
+        assertThatThrownBy {
+            InventoryManager(inventory)
+        }
+    }
+
+    @Test
+    fun `한 상품은 일반 상품 없이 프로모션 정보를 가질 수 없다`() {
+        var inventory = mutableListOf(
+            mutableMapOf(
+                ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "10",
+                ProductsColumn.PROMOTION to "dd"),
+        )
+        assertThatThrownBy {
+            InventoryManager(inventory)
+        }
+    }
+
+    @Test
+    fun `한 상품은 하나의 프로모션 정보를 가질 수 있다`() {
+        var inventory = mutableListOf(
+            mutableMapOf(
+                ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "10",
+                ProductsColumn.PROMOTION to "dd"),
+            mutableMapOf(
+                ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "10",
+                ProductsColumn.PROMOTION to "dd"),
+        )
+        assertThatThrownBy {
+            InventoryManager(inventory)
+        }
+    }
+
+    @Test
+    fun `상품의 중복을 허용하지 않는다`() {
+        var inventory = mutableListOf(
+            mutableMapOf(
+                ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "10",
+                ProductsColumn.PROMOTION to "null"),
+            mutableMapOf(
+                ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "12",
+                ProductsColumn.PROMOTION to "null"),
+        )
+        assertThatThrownBy {
+            InventoryManager(inventory)
+        }
+
+        inventory = mutableListOf(
+            mutableMapOf(
+                ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "10",
+                ProductsColumn.PROMOTION to "프로모션"),
+            mutableMapOf(
+                ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "12",
+                ProductsColumn.PROMOTION to "프로모션"),
+        )
+        assertThatThrownBy {
+            InventoryManager(inventory)
+        }
+    }
+
+    @Test
+    fun `각 상품의 재고 수량을 고려하여 결제 가능 여부를 확인한다` () {
+        var inventory = mutableListOf(
+            mutableMapOf(ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "0",
+                ProductsColumn.PROMOTION to "null"),
+            mutableMapOf(ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "10",
+                ProductsColumn.PROMOTION to "dd"),
+            )
+        var inventoryManager = InventoryManager(inventory)
+        var product = "콜라"
+        var isBuyable:Boolean = inventoryManager.checkProductAvailable(product)
+        assertThat(isBuyable).isFalse()
+    }
+
+    @Test
+    fun `고객이 상품을 구매할 때마다, 결제된 수량만큼 해당 상품의 재고에서 차감하여 수량을 관리한다`() {
+        var inventory = mutableListOf(
+            mutableMapOf(ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "10",
+                ProductsColumn.PROMOTION to "탄산2+1"),
+            mutableMapOf(ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "0",
+                ProductsColumn.PROMOTION to "null"),
+        )
+        var inventoryManager = InventoryManager(inventory)
+        var boughtProduct = "콜라"
+        var boughtCount = 2
+
+        var result = inventoryManager.deduct(boughtProduct, boughtCount, isPromotion = false)
+        assertThat(inventoryManager.inventory[0][ProductsColumn.QUANTITY]).isEqualTo("10")
+        assertThat(result).isEqualTo(InventoryManager.Msg.OUT_OF_STOCK)
+    }
+
+    @Test
+    fun `재고를 차감함으로써 시스템은 최신 재고 상태를 유지하며, 다음 고객이 구매할 때 정확한 재고 정보를 제공한다`() {
+        //java.io.File 로 파일을 덮어씌웁니다
+        var inventory = mutableListOf(
+            mutableMapOf(ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "10",
+                ProductsColumn.PROMOTION to "탄산2+1"),
+            mutableMapOf(ProductsColumn.NAME to "콜라",
+                ProductsColumn.PRICE to "1000",
+                ProductsColumn.QUANTITY to "10",
+                ProductsColumn.PROMOTION to "null"),
+        )
+        var inventoryManager = InventoryManager(inventory)
+        var CSVText = inventoryManager.inventoryToCSV()
+        assertThat(CSVText).contains("""
+            name,price,quantity,promotion
+            콜라,1000,10,탄산2+1
+            콜라,1000,10,null
+        """.trimIndent())
+
+    }
+
+    override fun runMain() {
+        main()
+    }
+}
+
+class InputViewTest : NsTest() {
 
     override fun runMain() {
         main()
